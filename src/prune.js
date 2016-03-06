5756e056dd75c884f8c53f99d7a55865d227f5b1
Jason Sebring <mail@jasonsebring.com>
2016-03-06 11:43:47 -0800

    tabs to spaces

--- src/prune.js
+++ src/prune.js
@@ -1,22 +1,22 @@
 'use strict';
 
 function validRectangle(rect) {
-	return rect && rect.top >= 0 && rect.left >= 0 && rect.width > 0 && rect.height > 0;
+  return rect && rect.top >= 0 && rect.left >= 0 && rect.width > 0 && rect.height > 0;
 }
 
 // remove unwanted items with offscreen top/left
 function prune(pageObj) {
-	Object.keys(pageObj).forEach(function(key) {
-		var arr = pageObj[key];
-		if (Array.isArray(arr) && arr.length && arr[0].rectangle)
-			for (var i = 0; i < arr.length; i++) {
-				if (!validRectangle(arr[i].rectangle)) {
-					arr.splice(i, 1);
-					i--;
-				}
-			}
-	});
-	return pageObj;
+  Object.keys(pageObj).forEach(function(key) {
+    var arr = pageObj[key];
+    if (Array.isArray(arr) && arr.length && arr[0].rectangle)
+      for (var i = 0; i < arr.length; i++) {
+        if (!validRectangle(arr[i].rectangle)) {
+          arr.splice(i, 1);
+          i--;
+        }
+      }
+  });
+  return pageObj;
 }
 
 module.exports = prune;
